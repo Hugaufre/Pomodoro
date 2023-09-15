7ce6246c175861becd926de87a9983e53a47f44a
Hugo <hugo.sefrioui@etu-unicaen.fr>
2023-09-15 21:25:01 +0200

    Update: Switch Work and Pause with colors change

--- Script/Script.js
+++ Script/Script.js
@@ -4,6 +4,7 @@ var workTime = 5
 var pauseTime = 5
 var refreshIntervalId
 var refreshIntervalId2
+var switchPW = false
 
 
 for(var i = 0; i < buttons.length; i++){
@@ -11,7 +12,7 @@ for(var i = 0; i < buttons.length; i++){
 
         if(!islaunched){
             document.getElementById("init").textContent = "REINITIALIZE"
-            document.getElementById("switchPauseWork").textContent = "WORK"
+            //document.getElementById("switchPauseWork").textContent = "WORK"
             islaunched = true
                 decreaseWorkTime()
         }else{
@@ -20,7 +21,7 @@ for(var i = 0; i < buttons.length; i++){
             clearInterval(refreshIntervalId)
             islaunched = false
             document.getElementById("init").textContent = "START"
-            document.getElementById("timer").textContent = 0 + ":"+ '00';
+            document.getElementById("timer").textContent = '00' + ":"+ '00';
         }
     })
 }
@@ -42,7 +43,7 @@ function decreaseWorkTime(){
         if(minutes == 0 && seconds == 0){
                 workTime = 5
                 clearInterval(refreshIntervalId)
-                document.getElementById("switchPauseWork").textContent = "PAUSE"
+                switchPauseWork()
                 decreasePauseTime()
             }
     }, 1_000)
@@ -67,11 +68,32 @@ function decreasePauseTime(){
 
      if(minutes == 0 && seconds == 0){ 
         pauseTime = 5
-        document.getElementById("switchPauseWork").textContent = "WORK"
         clearInterval(refreshIntervalId)
+        switchPauseWork()
         decreaseWorkTime()
         }
 
     }, 1_000)
 
+}
+
+function switchPauseWork(){
+
+    if(!switchPW){
+        document.getElementById("pauseID").style.backgroundColor = "green"
+        document.getElementById("pauseID").style.boxShadow = "2px 2px 5px 5px black"
+        document.getElementById("workID").style.backgroundColor = "black"
+        document.getElementById("workID").style.boxShadow = "none"
+        document.querySelector("body").style.backgroundColor = "green"
+        document.getElementById("init").style.backgroundColor = "green"
+        switchPW = true
+    }else{
+        document.getElementById("pauseID").style.backgroundColor = "black"
+        document.getElementById("pauseID").style.boxShadow = "none"
+        document.getElementById("workID").style.backgroundColor = "#c30303"
+        document.getElementById("workID").style.boxShadow = "2px 2px 5px 5px black"
+        document.querySelector("body").style.backgroundColor = "#c30303"
+        document.getElementById("init").style.backgroundColor = "#c30303"
+        switchPW = false
+    }
 }
